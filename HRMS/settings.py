605a12cc8d8457a35126c103da0ced8e76bf5d26
ReemAbdelhaleemHassan <es-reem.abdelhaleem2019@alexu.edu.eg>
2024-03-19 04:33:29 +0200

    connect to mysql

--- HRMS/settings.py
+++ HRMS/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'HRMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'HRMS',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '1234'
     }
 }
 
