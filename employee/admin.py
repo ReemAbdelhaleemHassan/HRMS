8270fb78b99103f4e32f0ffb436c0c33a5f2a767
ReemAbdelhaleemHassan <es-reem.abdelhaleem2019@alexu.edu.eg>
2024-03-22 17:10:31 +0200

    add contact infromation

--- employee/admin.py
+++ employee/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
-from .models import Location, PersonalInformation
+from .models import Location, PersonalInformation, ContactInformation
 
 # Register your models here.class LocationAdmin(admin.ModelAdmin):
-class LocationAdmin(admin.ModelAdmin):
-    list_display = ('location', 'department', 'section')
-    list_filter = ('location', 'department', 'section')
-    search_fields = ('location', 'department', 'section')
-
 class PersonalInformationAdmin(admin.ModelAdmin):
-    list_display = ('Name_in_Arabic', 'Name_in_English', 'display_location', 'section', 'department')
+    list_display = ('Name_in_Arabic', 'Name_in_English', 'display_location', 'section', 'department', 'marital_status', 'religion', 'national_id', 'expire_date', 'gender', 'date_of_birth', 'place_of_birth')
 
     def display_location(self, obj):
         return obj.location.location
@@ -23,8 +18,26 @@ class PersonalInformationAdmin(admin.ModelAdmin):
     section.short_description = 'Section'
     department.short_description = 'Department'
 
-    list_filter = ('location__section', 'location__department')
-    search_fields = ('Name_in_Arabic', 'Name_in_English', 'location__section', 'location__department')
+    list_filter = ('location__section', 'location__department', 'marital_status', 'religion', 'gender')
+    search_fields = ('Name_in_Arabic', 'Name_in_English', 'location__section', 'location__department', 'marital_status', 'religion', 'national_id', 'place_of_birth')
 
+
+class ContactInformationAdmin(admin.ModelAdmin):
+    list_display = ('get_employee_name', 'email_address', 'address', 'work_phone', 'mobile_no')
+    # list_filter = ('employee__employee_name',)  # Filter by employee name
+    search_fields = ('employee__employee_name', 'email_address')  # Enable searching by employee name and email address
+
+    def get_employee_name(self, obj):
+        return obj.employee.Name_in_English
+    get_employee_name.short_description = 'Employee Name'
+    
+    
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('location', 'department', 'section')
+    list_filter = ('location', 'department', 'section')
+    search_fields = ('location', 'department', 'section')
+    
+    
 admin.site.register(Location, LocationAdmin)
-admin.site.register(PersonalInformation, PersonalInformationAdmin)
+admin.site.register(PersonalInformation, PersonalInformationAdmin)    
+admin.site.register(ContactInformation, ContactInformationAdmin)
--- employee/models.py
+++ employee/models.py
@@ -11,10 +11,41 @@ class Location(models.Model):
         return f"{self.section} ({self.department}, {self.location})"
 
 class PersonalInformation(models.Model):
+    MARITAL_STATUS_CHOICES = [
+        ('single', 'Single'),
+        ('married', 'Married'),
+        ('divorced', 'Divorced'),
+        ('widow', 'Widow'),
+    ]
+
+    RELIGION_CHOICES = [
+        ('muslim', 'Muslim'),
+        ('christian', 'Christian'),
+    ]
+
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ]
+
     Name_in_Arabic = models.CharField(max_length=100, unique=True)
     Name_in_English = models.CharField(max_length=100, unique=True)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    marital_status = models.CharField(max_length=10, choices=MARITAL_STATUS_CHOICES, null=True)
+    religion = models.CharField(max_length=10, choices=RELIGION_CHOICES, null=True)
+    national_id = models.CharField(max_length=14, unique=True, null=True)
+    expire_date = models.DateField(null=True)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES, null=True)
+    date_of_birth = models.DateField(null=True) 
+    place_of_birth = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return f"{self.Name_in_Arabic} / {self.Name_in_English}"
     
+class ContactInformation(models.Model):
+    # Define your fields in the ContactInformation model
+    employee = models.ForeignKey(PersonalInformation, on_delete=models.CASCADE, primary_key=True)
+    email_address = models.EmailField(null=True)
+    address = models.CharField(max_length=255)
+    work_phone = models.CharField(max_length=20, null=True)
+    mobile_no = models.CharField(max_length=20)
