4717294d4c04159083be2688a829b2bc6fcce8d2
ReemAbdelhaleemHassan <es-reem.abdelhaleem2019@alexu.edu.eg>
2024-03-22 14:58:41 +0200

    add personal information & location

--- employee/admin.py
+++ employee/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Location, PersonalInformation
 
-# Register your models here.
+# Register your models here.class LocationAdmin(admin.ModelAdmin):
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('location', 'department', 'section')
+    list_filter = ('location', 'department', 'section')
+    search_fields = ('location', 'department', 'section')
+
+class PersonalInformationAdmin(admin.ModelAdmin):
+    list_display = ('Name_in_Arabic', 'Name_in_English', 'display_location', 'section', 'department')
+
+    def display_location(self, obj):
+        return obj.location.location
+    display_location.short_description = 'Location'
+    
+    def section(self, obj):
+        return obj.location.section
+    
+    def department(self, obj):
+        return obj.location.department
+
+    section.short_description = 'Section'
+    department.short_description = 'Department'
+
+    list_filter = ('location__section', 'location__department')
+    search_fields = ('Name_in_Arabic', 'Name_in_English', 'location__section', 'location__department')
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(PersonalInformation, PersonalInformationAdmin)
--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    location = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    section = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.section} ({self.department}, {self.location})"
+
+class PersonalInformation(models.Model):
+    Name_in_Arabic = models.CharField(max_length=100, unique=True)
+    Name_in_English = models.CharField(max_length=100, unique=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.Name_in_Arabic} / {self.Name_in_English}"
+    
